b2630f6856f24c7a3df208577bf3fd940a508e47
ucpeter <chucks0606@gmail.com>
2026-01-29 08:51:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -326,7 +326,7 @@ async function convertUSDToTokenAmount(network, tokenSymbol, usdAmount) {
     return {
       tokenAmount,
       tokenPrice,
-      tokenAmountWei: ethers.parseUnits(tokenAmount.toString(), tokenData.decimals),
+      tokenAmountWei: ethers.parseUnits(tokenAmount.toFixed(8), tokenData.decimals),
       source: 'live_price'
     };
   } catch (error) {
